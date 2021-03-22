0b2e1b8bc10a984a88d1edd9733e6562e27697df
Maria Clara Couto Lorena <mariaclaracoutolorena@gmail.com>
2021-03-22 13:03:18 -0300

    "integrando ballPosition no Vision"

--- vision.py
+++ vision.py
@@ -1,9 +1,42 @@
-# Import the camera server
+# Import the camera server, OpenCV, NumPy and argparse
 from cscore import CameraServer
-
-# Import OpenCV and NumPy
 import cv2
 import numpy as np
+import argparse
+
+def ballPosition(frame):
+    frameSizeX = len(frame[1])
+    yellowLower = (20, 100, 100)
+    yellowUpper = (30, 255, 255)
+    
+    # resize the frame, blur it, and convert it to the HSV color space
+    blurred = cv2.GaussianBlur(frame, (11, 11), 0)
+    hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
+    mask = cv2.inRange(hsv, yellowLower, yellowUpper)
+    # perform a series of dilations and erosions to remove any small blobs left in the mask
+    mask = cv2.erode(mask, None, iterations=2)
+    mask = cv2.dilate(mask, None, iterations=2)
+
+    # find contours in the mask and initialize the current
+    cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
+    cv2.CHAIN_APPROX_SIMPLE)[-2]
+    center = None
+
+    # only proceed if at least one contour was found
+    if len(cnts) > 0:
+    c = max(cnts, key=cv2.contourArea)
+    ((x, y), circleRadius) = cv2.minEnclosingCircle(c)
+    M = cv2.moments(c)
+    center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+
+    if circleRadius > 20:
+        # then update the list of tracked points
+        if(center):
+            centerX = center[0]
+            relativeX = centerX / frameSizeX
+            relativeRadius = circleRadius/frameSizeX   
+            return relativeX, relativeRadius
+        return None
 
 def main():
     cs = CameraServer.getInstance()
@@ -32,9 +65,6 @@ def main():
             # skip the rest of the current iteration
             continue
 
-        #
         # Insert your image processing logic here!
-        #
-
-        # (optional) send some image back to the dashboard
-        outputStream.putFrame(img)
+        relativeX, relativeRadius = ballPosition(img)
+        
